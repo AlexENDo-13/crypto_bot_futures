cfda33488147d783434f71bdbf6e3eba805038b4
AlexENDo-13 <kozhanov200@gmail.com>
2026-04-23 13:13:08 +0300

    fix: update

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# src/utils/__init__.py
 
-from src.utils.api_client import BingXClient, AsyncBingXClient
+from src.utils.api_client import AsyncBingXClient
+from src.utils.state_manager import StateManager
+from src.utils.auto_recovery import AutoRecovery
+from src.utils.cache_manager import CacheManager
+from src.utils.performance_metrics import PerformanceMetrics
+from src.utils.profiler import Profiler
+from src.utils.sqlite_history import SQLiteTradeHistory
+from src.utils.ai_exporter import AIExporter
 
-__all__ = ["BingXClient", "AsyncBingXClient"]
-from .sqlite_history import AsyncSQLiteTradeHistory
+__all__ = [
+    "AsyncBingXClient",
+    "StateManager",
+    "AutoRecovery",
+    "CacheManager",
+    "PerformanceMetrics",
+    "Profiler",
+    "SQLiteTradeHistory",
+    "AIExporter",
+]
